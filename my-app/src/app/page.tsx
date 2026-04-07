a0d962823607e0db765331fd91a6eb25479c123b
Mikołaj Michalak <majkanart117@gmail.com>
2026-04-07 18:51:19 +0200

    feat:Add Api for free json.

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,18 @@
-import { useState} from "react"
-export default function Home() {
-  
+export default async function Home() {
+
+  interface Post {
+    id: number;
+    title: string;
+  }
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const res: Post[] = await data.json();
   return (
     <main className="max-w-4xl mx-auto p-8">
       <h1 className="text-3xl font-bold">Sports App</h1>
       <p className="text-gray-500 mt-2">Browse leagues and teams</p>
+      {res.map((item)=>(
+        <p key={item.id}>{item.title}</p>
+      ))}
     </main>
   )
 }
