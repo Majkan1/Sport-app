d88c94ea60ebfa61c0b9ca759481963c1906400f
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-07 18:13:12 +0200

    delete: the favourute one

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,6 @@ import Link from "next/link"
     <nav className="flex justify-center gap-4">
       <Link href="/">SportsApp</Link>
       <Link href="/about">About</Link>
-      <p>Favourite one:{}</p>
     </nav>
   )
 }
--- src/app/components/SearchWrapper.tsx
+++ src/app/components/SearchWrapper.tsx
@@ -20,10 +20,6 @@ export default function SearchWrapper({leagues = []}:{leagues?:League[] | null})
               <Link href={`/leagues/${item.idLeague}`}>
                 {item.strLeague} 
               </Link>
-              <button className="m-1 bg-amber-300 rounded-xl p-1">
-                Add to the favourite
-                <input type="checkbox" className="m-1" />
-              </button>
             </div>
           ))
         ) : (
